e3739ee93a56eb75474faa23826f0f42a9a810ac
nicolas <nicolas.arthur2786@gmail.com>
2022-07-19 21:58:37 -0300

    Fazendo get por id do cliente

--- src/main/java/br/vendas/nickart/controllers/ClienteController.java
+++ src/main/java/br/vendas/nickart/controllers/ClienteController.java
@@ -1,19 +1,33 @@
 package br.vendas.nickart.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import br.vendas.nickart.entities.Cliente;
+import br.vendas.nickart.repositories.Clientes;
+
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 	
-	@RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+	@Autowired
+	Clientes clientes;
+	
+	@GetMapping("/{id}")
 	@ResponseBody
-	public String helloCliente(@PathVariable("nome") String nomeCliente) {
-		return String.format("Hello %s ", nomeCliente);
+	public ResponseEntity getClienteById(@PathVariable("id") Integer id) {
+		Optional<Cliente> cliente = clientes.findById(id);
+		
+		
+		return cliente.isPresent() ? ResponseEntity.ok(cliente .get()) : ResponseEntity.notFound().build();
 	}
 	
 }
